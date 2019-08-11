df6b24cf3129607dc80fb26bece1341716a41d7d
steliosk9 <steliosk9@gmail.com>
2019-08-11 11:20:44 -0500

    initial init

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var db = require('./models');
+
+var app = express();
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers");
+
+app.use(routes);
+
+// listen on port 3000
+var PORT = process.env.PORT || 3000;
+db.sequelize.sync().then(function(){
+			app.listen(PORT, function() {
+			  console.log("App listening on PORT " + PORT);
+			});
+		});
