d8529343c8ca884cba8337cb079dee4203174172
steliosk9 <steliosk9@gmail.com>
2019-10-10 14:35:29 -0500

    server.js

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 var express = require("express");
-var db = require('./models');
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var PORT = process.env.PORT || 8080;
+var app = express(); 
 
-var app = express();
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+var db = require("./models"); 
 
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Sets up the Express app to handle data parsing
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
 
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({
-  defaultLayout: "main"
-}));
+//app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controllers");
+var burgerRoutes = require("./controllers/burgers_controller.js"); 
 
-app.use(routes);
+app.use(burgerRoutes);
 
-// listen on port 3000
-var PORT = process.env.PORT || 3000;
-db.sequelize.sync().then(function(){
-			app.listen(PORT, function() {
-			  console.log("App listening on PORT " + PORT);
-			});
-		});
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
